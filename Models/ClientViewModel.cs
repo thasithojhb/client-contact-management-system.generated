da639dec2482a67172a35e8600b18e7d1f8d19cf
thato <thato@LAPTOP-BPECFT9F>
2024-08-28 21:39:21 +0200

    Add project files.

---
+++ Models/ClientViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace client_contact_management_system.Models
+{
+    public class ClientViewModel
+    {
+        public required string Name { get; set; }
+        public required string ClientCode { get; set; }
+        public List<ContactViewModel>? Contacts { get; set; }
+    }
+}
---
+++ Models/ContactViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace client_contact_management_system.Models
+{
+    public class ContactViewModel
+    {
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string Email { get; set; }
+        public List<ClientViewModel>? Contacts { get; set; }
+    }
+}
